82986306a4d51ee90202c8a59992f1721d5b6e21
nuex <nx@nu-ex.com>
2014-10-10 13:12:17 -0400

    support for path templates

--- src/prop_otp.erl
+++ src/prop_otp.erl
@@ -17,7 +17,8 @@ generate(Options) ->
   dir("apps"),
   dir("priv"),
   dir("ebin"),
-  template("README.md", Options).
+  template("README.md", Options),
+  template("ebin/app.app", "ebin/{{name}}.app", Options).
 
 %% ===================================================================
 %% Behaviour Functions
@@ -33,18 +34,21 @@ chdir(Directory) -> file:set_cwd(Directory).
 dir(Directory) -> ensure_directory(filelib:is_dir(Directory), Directory).
 
 %% Render a template
-template(Name, Context) ->
-  ModuleAttributes = ?MODULE:module_info(attributes),
-  [Generator | _Rest] = proplists:get_value(prop, ModuleAttributes),
-  ModuleDirectory = filename:dirname(code:which(?MODULE)),
-  Path = filename:join([ModuleDirectory, "..", "priv",
-                        "templates", erlang:atom_to_list(Generator),
-                        lists:concat([Name, ".mustache"])]),
-  {ok, Raw} = file:read_file(Path),
+template(OutputPath, Context) ->
+  {ok, Raw} = read_template(OutputPath),
   {ok, Template} = elk:compile(Raw),
   Rendered = elk:render(Template, {proplist, binary_keys(Context)}),
-  file:write_file(Name, Rendered).
+  file:write_file(OutputPath, Rendered).
 
+%% Render a template with a different output path
+template(TemplatePath, RawOutputPath, Context) ->
+  {ok, PathTemplate} = elk:compile(erlang:list_to_binary(RawOutputPath)),
+  RenderedPath = elk:render(PathTemplate, {proplist, binary_keys(Context)}),
+  {ok, RawFileTemplate} = read_template(TemplatePath),
+  {ok, FileTemplate} = elk:compile(RawFileTemplate),
+  Rendered = elk:render(FileTemplate, {proplist, binary_keys(Context)}),
+  file:write_file(RenderedPath, Rendered).
+  
 %% ===================================================================
 %% Private Functions
 %% ===================================================================
@@ -52,8 +56,20 @@ template(Name, Context) ->
 %% Make all keys binaries
 binary_keys(Context) ->
   [{erlang:list_to_binary(erlang:atom_to_list(Key)), Value} ||
-      {Key, Value} <- Context ].
+      {Key, Value} <- Context].
 
 %% Make a directory if it doesn't exist
 ensure_directory(true, _Directory) -> ok;
 ensure_directory(false, Directory) -> file:make_dir(Directory).
+
+generator_path() ->
+  ModuleAttributes = ?MODULE:module_info(attributes),
+  [Generator | _Rest] = proplists:get_value(prop, ModuleAttributes),
+  ModuleDirectory = filename:dirname(code:which(?MODULE)),
+  filename:join([ModuleDirectory, "..", "priv", "generators",
+                 erlang:atom_to_list(Generator)]).
+
+read_template(RelativeTemplatePath) ->
+  Path = filename:join([generator_path(), "templates", 
+                        lists:concat([RelativeTemplatePath, ".mustache"])]),
+  file:read_file(Path).
