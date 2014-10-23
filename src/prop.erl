a4a87ea1dbef235728386f8f56c7095a77612a6b
nuex <nx@nu-ex.com>
2014-10-23 10:57:13 -0400

    make prop module a prop generator behaviour

--- src/prop.erl
+++ src/prop.erl
@@ -1,5 +1,9 @@
 -module(prop).
--export([generate/2]).
+-export([generate/2, attr/2, chdir/1, dir/1, template/3, template/4]).
+-export([behaviour_info/1]).
+
+behaviour_info(callbacks) -> [{generate, 1}];
+behaviour_info(_) -> undefined.
 
 generate(Generator, Options) ->
   Mod = find_generator(Generator),
@@ -31,3 +35,57 @@ is_generator(Mod, Generator) ->
 is_prop_module(Module) ->
   Name = erlang:atom_to_list(Module),
   (re:run(Name, "^prop_") /= nomatch).
+
+%% ===================================================================
+%% Behaviour Functions
+%% ===================================================================
+
+%% Fetch an attribute from passed in options
+attr(Key, Options) -> proplists:get_value(Key, Options).
+
+%% Change directory
+chdir(Directory) -> file:set_cwd(Directory).
+
+%% Ensure a directory exists
+dir(Directory) -> ensure_directory(filelib:is_dir(Directory), Directory).
+
+%% Render a template
+template(Mod, OutputPath, Context) ->
+  {ok, Raw} = read_template(Mod, OutputPath),
+  {ok, Template} = elk:compile(Raw),
+  Rendered = elk:render(Template, {proplist, binary_keys(Context)}),
+  file:write_file(OutputPath, Rendered).
+
+%% Render a template with a different output path
+template(Mod, TemplatePath, RawOutputPath, Context) ->
+  {ok, PathTemplate} = elk:compile(erlang:list_to_binary(RawOutputPath)),
+  RenderedPath = elk:render(PathTemplate, {proplist, binary_keys(Context)}),
+  {ok, RawFileTemplate} = read_template(Mod, TemplatePath),
+  {ok, FileTemplate} = elk:compile(RawFileTemplate),
+  Rendered = elk:render(FileTemplate, {proplist, binary_keys(Context)}),
+  file:write_file(RenderedPath, Rendered).
+  
+%% ===================================================================
+%% Private Functions
+%% ===================================================================
+
+%% Make all keys binaries
+binary_keys(Context) ->
+  [{erlang:list_to_binary(erlang:atom_to_list(Key)), Value} ||
+      {Key, Value} <- Context].
+
+%% Make a directory if it doesn't exist
+ensure_directory(true, _Directory) -> ok;
+ensure_directory(false, Directory) -> file:make_dir(Directory).
+
+generator_path(Mod) ->
+  ModuleAttributes = Mod:module_info(attributes),
+  [Generator | _Rest] = proplists:get_value(prop, ModuleAttributes),
+  ModuleDirectory = filename:dirname(code:which(Mod)),
+  filename:join([ModuleDirectory, "..", "priv", "generators",
+                 erlang:atom_to_list(Generator)]).
+
+read_template(Mod, RelativeTemplatePath) ->
+  Path = filename:join([generator_path(Mod), "templates", 
+                        lists:concat([RelativeTemplatePath, ".mustache"])]),
+  file:read_file(Path).
--- src/prop_otp.erl
+++ src/prop_otp.erl
@@ -1,75 +1,22 @@
 -module(prop_otp).
 -export([generate/1]).
+-behaviour(prop).
 -prop(otp).
 
 %% ===================================================================
 %% API
 %% ===================================================================
 
-%% Generate an 
+%% Generate an OTP release skeleton
 generate(Options) ->
-  OutputDirectory = attr(output_directory, Options),
-  Name = attr(name, Options),
+  {ok, OutputDirectory} = file:get_cwd(),
+  Name = prop:attr(name, Options),
   TargetDirectory = filename:join([OutputDirectory, Name]),
-  dir(TargetDirectory),
-  chdir(TargetDirectory),
-  dir("config"),
-  dir("apps"),
-  dir("priv"),
-  dir("ebin"),
-  template("README.md", Options),
-  template("ebin/app.app", "ebin/{{name}}.app", Options).
-
-%% ===================================================================
-%% Behaviour Functions
-%% ===================================================================
-
-%% Fetch an attribute from passed in options
-attr(Key, Options) -> proplists:get_value(Key, Options).
-
-%% Change directory
-chdir(Directory) -> file:set_cwd(Directory).
-
-%% Ensure a directory exists
-dir(Directory) -> ensure_directory(filelib:is_dir(Directory), Directory).
-
-%% Render a template
-template(OutputPath, Context) ->
-  {ok, Raw} = read_template(OutputPath),
-  {ok, Template} = elk:compile(Raw),
-  Rendered = elk:render(Template, {proplist, binary_keys(Context)}),
-  file:write_file(OutputPath, Rendered).
-
-%% Render a template with a different output path
-template(TemplatePath, RawOutputPath, Context) ->
-  {ok, PathTemplate} = elk:compile(erlang:list_to_binary(RawOutputPath)),
-  RenderedPath = elk:render(PathTemplate, {proplist, binary_keys(Context)}),
-  {ok, RawFileTemplate} = read_template(TemplatePath),
-  {ok, FileTemplate} = elk:compile(RawFileTemplate),
-  Rendered = elk:render(FileTemplate, {proplist, binary_keys(Context)}),
-  file:write_file(RenderedPath, Rendered).
-  
-%% ===================================================================
-%% Private Functions
-%% ===================================================================
-
-%% Make all keys binaries
-binary_keys(Context) ->
-  [{erlang:list_to_binary(erlang:atom_to_list(Key)), Value} ||
-      {Key, Value} <- Context].
-
-%% Make a directory if it doesn't exist
-ensure_directory(true, _Directory) -> ok;
-ensure_directory(false, Directory) -> file:make_dir(Directory).
-
-generator_path() ->
-  ModuleAttributes = ?MODULE:module_info(attributes),
-  [Generator | _Rest] = proplists:get_value(prop, ModuleAttributes),
-  ModuleDirectory = filename:dirname(code:which(?MODULE)),
-  filename:join([ModuleDirectory, "..", "priv", "generators",
-                 erlang:atom_to_list(Generator)]).
-
-read_template(RelativeTemplatePath) ->
-  Path = filename:join([generator_path(), "templates", 
-                        lists:concat([RelativeTemplatePath, ".mustache"])]),
-  file:read_file(Path).
+  ok = prop:dir(TargetDirectory),
+  ok = prop:chdir(TargetDirectory),
+  ok = prop:dir("config"),
+  ok = prop:dir("apps"),
+  ok = prop:dir("priv"),
+  ok = prop:dir("ebin"),
+  ok = prop:template(?MODULE, "README.md", Options),
+  ok = prop:template(?MODULE, "ebin/app.app", "ebin/{{name}}.app", Options).
