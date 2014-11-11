667653875daff2b3f9f5c84abee54cdadc4f1001
nuex <nx@nu-ex.com>
2014-11-11 10:16:41 -0500

    generate a simple list of available generators

--- src/prop_cli.erl
+++ src/prop_cli.erl
@@ -14,6 +14,9 @@ main(Args) ->
 %% PRIVATE FUNCTIONS
 %% ===================================================================
 
+atomize(String) -> erlang:list_to_atom(String).
+
+%% Generate something
 command(new, [Name | Args]) ->
   GeneratorName = generator_name(re:split(Name, ":")),
   Generator = prop:find_generator(GeneratorName),
@@ -23,6 +26,9 @@ command(new, [Name | Args]) ->
   ExtraOptions = [{name, ResourceName}, {invocation, command_line},
                   {module, Generator}],
   Generator:generate(lists:append([ExtraOptions, Options]));
+%% List installed generators
+command(list, []) ->
+  [info_line(Generator) || Generator <- prop:generators()];
 command(Unknown, _Args) ->
   io:format("unknown command ~p~n", [Unknown]).
 
@@ -32,12 +38,11 @@ generator_name(NameArgs) ->
   AtomizedNames = [erlang:binary_to_atom(X, utf8) || X <- NameArgs],
   erlang:list_to_tuple(AtomizedNames).
 
+%% Display a description for a generator for the list command
+info_line(Generator) ->
+  PrettyName = prop:formatted_name(Generator),
+  io:format("prop new ~s - ~s~n", [PrettyName, Generator:description()]).
+
 %% TODO(nuex): add usage info
 usage() ->
   io:format("usage: prop [command] [generator] [key]~n", []).
-
-%%
-%% PRIVATE FUNCTIONS
-%%
-
-atomize(String) -> erlang:list_to_atom(String).
