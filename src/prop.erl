3b8b2b4a8bf1495c0bcc8ed22b4e09dd2f6792b7
nuex <nx@nu-ex.com>
2014-11-21 03:37:16 -0500

    default to announce, fix generator finding, add destination function for creating and chdiring directory

--- src/prop.erl
+++ src/prop.erl
@@ -1,6 +1,6 @@
 -module(prop).
--export([attr/2, chdir/1, dir/3, exec/3, find_generator/1, generate/2,
-         generators/0, template/3, template/4]).
+-export([attr/2, chdir/1, destination/2, dir/3, exec/3, find_generator/1,
+         generate/2, generators/0, template/3, template/4]).
 
 -callback generate(list()) -> any().
 -callback name() -> atom().
@@ -12,8 +12,12 @@
 %% ===================================================================
 
 find_generator(Generator) ->
-  Found = [Mod || Mod <- generators(), Mod:name() == Generator],
-  erlang:hd(Found).
+  case [Mod || Mod <- generators(), Mod:name() == Generator] of
+    [] ->
+      io:format("Unable to find generator ~p.~n", [Generator]),
+      erlang:error(generator_not_found);
+    Found -> erlang:hd(Found)
+  end.
 
 generate(Generator, Options) ->
   Mod = find_generator(Generator),
@@ -30,48 +34,45 @@ generators() ->
 %% Fetch an attribute from passed in options
 attr(Prop, Key) -> proplists:get_value(Key, Prop).
 
+%% Create the destination directory and change to it
+destination(Prop, Directory) ->
+  dir(Prop, Directory, [{announce, false}]),
+  chdir(Directory).
+
 %% Change directory
 chdir(Directory) -> file:set_cwd(Directory).
 
 %% Ensure a directory exists
 dir(Prop, Directory, Options) ->
   Invocation = proplists:get_value(invocation, Prop),
-  Announcing = proplists:get_value(announce, Options),
   CliMessage = "  " ++ color(success) ++ "create" ++ color_reset() ++ " ~s~n",
+  Announcing = is_announcing(Options),
   announce(Announcing, Invocation, {CliMessage, [Directory]}),
   ensure_directory(filelib:is_dir(Directory), Directory).
 
 %% Run a command
 exec(Prop, Command, Options) ->
   Invocation = proplists:get_value(invocation, Prop),
-  Announcing = lists:member(announce, Options),
   CliMessage = "  " ++ color(success) ++ "  exec" ++ color_reset() ++ " ~s~n",
+  Announcing = is_announcing(Options),
   announce(Announcing, Invocation, {CliMessage, [Command]}),
   Result = os:cmd(Command),
   io:format(Result, []),
   ok.
 
 %% Render a template
-template(Prop, OutputPath, Options) ->
-  Mod = proplists:get_value(module, Prop),
-  Invocation = proplists:get_value(invocation, Prop),
-  Announcing = lists:member(announce, Options),
-  CliMessage = "  " ++ color(success) ++ "create" ++ color_reset() ++ " ~s~n",
-  announce(Announcing, Invocation, {CliMessage, [OutputPath]}),
-  {ok, Raw} = read_template(Mod, OutputPath),
-  {ok, Template} = elk:compile(Raw),
-  Rendered = elk:render(Template, {proplist, binary_keys(Prop)}),
-  file:write_file(OutputPath, Rendered).
+template(Prop, TemplatePath, RawOutputPath) ->
+  template(Prop, TemplatePath, RawOutputPath, []).
 
 %% Render a template with a different output path
 template(Prop, TemplatePath, RawOutputPath, Options) ->
   Mod = proplists:get_value(module, Prop),
   Invocation = proplists:get_value(invocation, Prop),
-  Announcing = lists:member(announce, Options),
   {ok, PathTemplate} = elk:compile(erlang:list_to_binary(RawOutputPath)),
   RenderedPath = elk:render(PathTemplate, {proplist, binary_keys(Prop)}),
   RenderedPathForAnnounce = erlang:binary_to_list(RenderedPath),
   CliMessage = "  " ++ color(success) ++ "create" ++ color_reset() ++ " ~s~n",
+  Announcing = is_announcing(Options),
   announce(Announcing, Invocation, {CliMessage, [RenderedPathForAnnounce]}),
   {ok, RawFileTemplate} = read_template(Mod, TemplatePath),
   {ok, FileTemplate} = elk:compile(RawFileTemplate),
@@ -81,6 +82,11 @@ template(Prop, TemplatePath, RawOutputPath, Options) ->
 %% ===================================================================
 %% Private Functions
 %% ===================================================================
+
+announce(true, command_line, {Message, Options}) ->
+  io:format(Message, Options);
+announce(true, _Invocation, _Options) -> ok;
+announce(false, _Invocation, _Options) -> ok.
 
 %% Make all keys binaries
 binary_keys(Context) ->
@@ -100,6 +106,15 @@ color_reset() -> "\e[0m".
 ensure_directory(true, _Directory) -> ok;
 ensure_directory(false, Directory) -> file:make_dir(Directory).
 
+%% Load modules related to prop
+ensure_generators_loaded() ->
+  [ensure_loaded(F) || P <- code:get_path(),
+                       F <- filelib:wildcard(P ++ "/prop_*.beam")].
+
+ensure_loaded(File) ->
+  Path = erlang:list_to_atom(filename:rootname(filename:basename(File))),
+  code:ensure_loaded(Path).
+
 generator_name_to_path(Name) when erlang:is_atom(Name) ->
   erlang:atom_to_list(Name);
 generator_name_to_path(Name) when erlang:is_tuple(Name) ->
@@ -111,19 +126,10 @@ generator_path(Generator) ->
   filename:join([GeneratoruleDirectory, "..", "priv", "generators",
                  generator_name_to_path(Name)]).
 
-read_template(Mod, RelativeTemplatePath) ->
-  Path = filename:join([generator_path(Mod), "templates", 
-                        lists:concat([RelativeTemplatePath, ".mustache"])]),
-  file:read_file(Path).
-
-%% Load modules related to prop
-ensure_generators_loaded() ->
-  [ensure_loaded(F) || P <- code:get_path(),
-                       F <- filelib:wildcard(P ++ "/prop_*.beam")].
-
-ensure_loaded(File) ->
-  Path = erlang:list_to_atom(filename:rootname(filename:basename(File))),
-  code:ensure_loaded(Path).
+is_announcing(Options) when is_list(Options) ->
+  is_announcing(proplists:get_value(announce, Options));
+is_announcing(undefined) -> true;
+is_announcing(Boolean) -> Boolean.
 
 is_generator(Mod) ->
   Exports = Mod:module_info(exports),
@@ -135,7 +141,8 @@ is_prop_module(Module) ->
   Name = erlang:atom_to_list(Module),
   re:run(Name, "^prop_") /= nomatch.
 
-announce(true, command_line, {Message, Options}) ->
-  io:format(Message, Options);
-announce(true, _Invocation, _Options) -> ok;
-announce(false, _Invocation, _Options) -> ok.
+read_template(Mod, RelativeTemplatePath) ->
+  Path = filename:join([generator_path(Mod), "templates", 
+                        lists:concat([RelativeTemplatePath, ".mustache"])]),
+  file:read_file(Path).
+
